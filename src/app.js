fd685170f23a528dc31aee6557de821c5831aad8
Nandish Rao <nandishhraoo@gmail.com>
2026-04-03 22:42:00 +0530

    init : http server setup
    Signed-off-by: Nandish Rao <nandishhraoo@gmail.com>

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express =  require("express")
+
+const app = express()
+const PORT = 3000;
+
+const http = require("http");
+
+const server = http.createServer(app);
+server.listen(PORT , ()=>{
+    console.log(`server running on port ${PORT}🚀🚀🚀`)
+});
+
+app.get("/",(req,res)=>{
+    res.send("Backend is running")
+})
