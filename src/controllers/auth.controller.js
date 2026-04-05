ab05693efcb6d2ef6b504731484ce52042f9c8ee
Nandish Rao <nandishhraoo@gmail.com>
2026-04-05 13:28:32 +0530

    added signup, login and me methods for auth
    Signed-off-by: Nandish Rao <nandishhraoo@gmail.com>

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/dbModel";
-import { errorResponse, successResponse } from "../utils";
+import { errorResponse, generateToken, successResponse } from "../utils";
+import bcrypt from 'bcrypt';
 
 export const signUp = async(req,res)=>{
     const payload = req.body;
@@ -30,7 +31,59 @@ export const signUp = async(req,res)=>{
         }
         return successResponse(res, userResponse,201)
     }catch(err){
-
+        console.log(err)
+        return errorResponse(err,"Something went wrong",500)
     }       
 
+}
+
+export const login = async (req,res)=>{
+    const payload = req.body;
+
+    try{
+        //we have the user data if the user exist
+        const user = await User.findOne({
+            email  : payload.email
+        })
+        if(!user){
+            return errorResponse(res , "User not found" , 400);
+        }
+
+        const isPasswordValid = bcrypt.compare(payload.password , user.password)
+        if(!isPasswordValid) return errorResponse(res, "Invalid Credentials" , 400)
+            const jwtpayload = jwtpayload={
+                 userId: user._id.toString(),
+                 role: user.role,
+        }
+        const token = generateToken(jwtpayload)
+        return successResponse(res, {token} )
+
+    }catch(err){
+        console.log("ERROR: ",err)
+        return errorResponse(err , "SERVER ERROR",500);
+    }
+}
+
+export const me = async(req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findOne({
+            userId : userId
+        })
+        if(!user){
+            return errorResponse(err , "User not found" ,400)
+        }
+        const userObject = user.toObject();
+        const userResponse={
+              _id: userObject._id.toString(),
+            name: userObject.name,
+            email: userObject.email,
+            role: userObject.role,
+        }
+
+        return successResponse(res , userResponse)
+    }catch(err){
+        console.log(err);
+        return errorResponse(res, "Internal server error", 500);
+    }
 }
