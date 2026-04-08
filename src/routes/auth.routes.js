4e81278519814bbec41fa7a4cbe1c4d9f9090f35
Nandish Rao <nandishhraoo@gmail.com>
2026-04-08 22:13:37 +0530

    changes in the imports
    Signed-off-by: Nandish Rao <nandishhraoo@gmail.com>

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+import authController from "../controllers/auth.controller.js"
+const router = Router();
+
+router.post('/login', authController.login);    
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes.js';
-import classRoutes from './class.routes.js';
+// import classRoutes from './class.routes.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-router.use('/', classRoutes);
+// router.use('/', classRoutes);
 
 export default router;
