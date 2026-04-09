c4a445099844794629899f838e572dd8de6d4433
Nandish Rao <nandishhraoo@gmail.com>
2026-04-09 19:03:32 +0530

    add : userAuth (could be anyone teacher or student)
    Signed-off-by: Nandish Rao <nandishhraoo@gmail.com>

---
+++ src/midddlewares/middleware.js
@@ -0,0 +1,29 @@
+import { errorResponse , verifytoken } from "../utils";
+import { User } from '../models/schema.js';
+import { TokenExpiredError } from "jsonwebtoken";
+
+export const authMiddleware = async(req , res ,next)=>{
+    const token = req.headers?.authorization;
+    console.log(`the token in the headers is ${token} `)
+
+    if(!token){
+        return errorResponse(res , "Unauthorized! token is missing or invalid",401);
+    }
+    try{
+        const decoded = verifytoken(token)
+        if(!decoded){
+            return errorResponse(res, "Unauthorized! token is missing or invalid" , 401)
+        }
+        const userId = decoded.userId;
+        const user = await User.findById(userId)
+        if(!user){
+            return errorResponse(res , "User does not exist", 401)
+        }
+        req.user = decoded;
+        console.log("Decoded token" + decoded)
+        next();
+    }catch(err){
+         console.log(error);
+        return errorResponse(res, "Unauthorized, token missing or invalid", 401);
+    }
+}
